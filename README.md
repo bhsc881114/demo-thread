c94ee8304e4c2f60c010352417c8f9a4928b06ea
chentao <bhsc.happy@gmail.com>
2016-08-18 23:41:15 +0800

    java thread

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# algorithm
-
-* **Short62Util:** number to short id（9998-->qLc），make QR code simple or save space. high performance and thread safe
-
-# common 
-* **EnvUtil:** judge env,os...etc
-* **MailUtil:**  send mail tips， especially alarm
+# java thread demo
--- src/main/java/com/bhsc/demo/thread/WtfThread.java
+++ src/main/java/com/bhsc/demo/thread/WtfThread.java
@@ -3,5 +3,73 @@ package com.bhsc.demo.thread;
 
 public class WtfThread {
 
+  public static void main(String[] args) throws InterruptedException {
+
+    yieldThread();
+    blockWait();
+    blockLock();
+
+    System.out.println("hello:" + Thread.currentThread().getName());
+    Thread.sleep(10000000);
+  }
+
+  public static void blockWait() {
+    final Object waitObj = new Object();
+    new Thread("waitThread") {
+      public void run() {
+        try {
+          synchronized (waitObj) {
+            waitObj.wait();
+          }
+        } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+    }.start();
+  }
+
+  public static void blockLock() throws InterruptedException {
+    final Object waitObj = new Object();
+    new Thread("ownerThread") {
+      public void run() {
+        synchronized (waitObj) {
+          try {
+            while (true) {
+              Thread.sleep(1000);
+            }
+          } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+        }
+      }
+    }.start();
+
+    Thread.sleep(1000);
+
+    new Thread("blockThread") {
+      public void run() {
+        synchronized (waitObj) {
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+        }
+      }
+    }.start();
+  }
+
+  public static void yieldThread() {
+    new Thread("yieldThread") {
+      public void run() {
+        while (true) {
+          Thread.currentThread().yield();
+        }
+      }
+    }.start();
+  }
 
 }
